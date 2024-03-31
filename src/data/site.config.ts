3e7039092433cee4043ca9feb229668e505659cf
AhmedMuhsingez <76662243+AhmedMuhsingez@users.noreply.github.com>
2024-03-31 15:39:52 +0300

    commit

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,10 +9,10 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
-	lang: 'en-GB',
+	author: 'TechnoDroid', // Site author
+	title: 'TechnoDroid', // Site title.
+	description: '', // Description to display in the meta tags
+	lang: '',
 	ogLocale: 'en_GB',
 	shareMessage: 'Share this post', // Message to share a post on social media
 	paginationSize: 6 // Number of posts per page
