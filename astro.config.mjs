e34b15bb866dd37d48c5efaf3733ca34ace5a524
AhmedMuhsingez <76662243+AhmedMuhsingez@users.noreply.github.com>
2024-04-20 16:43:02 +0300

    try 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import { remarkReadingTime } from './src/utils/readTime.ts'
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel/static'
 
 // https://astro.build/config
 export default defineConfig({
