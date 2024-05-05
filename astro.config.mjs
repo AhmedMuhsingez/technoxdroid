76ba8f3dc0d5c99c9044886f6171feaf96f665d9
AhmedMuhsingez <76662243+AhmedMuhsingez@users.noreply.github.com>
2024-05-05 16:19:10 +0300

    added date to card

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,39 +1,41 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import { remarkReadingTime } from './src/utils/readTime.ts';
-import vercel from '@astrojs/vercel/serverless';
-
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import { remarkReadingTime } from './src/utils/readTime.ts'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
-  site: 'https://technoxdroid.com/',
-  // Write here your website url
-  markdown: {
-    remarkPlugins: [remarkReadingTime],
-    drafts: true,
-    shikiConfig: {
-      theme: 'material-theme-palenight',
-      wrap: true
-    }
-  },
-  image: {
-    service: {
-      entrypoint: 'astro/assets/services/noop'
-    }
-  },
-  integrations: [mdx({
-    syntaxHighlight: 'shiki',
-    shikiConfig: {
-      experimentalThemes: {
-        light: 'vitesse-light',
-        dark: 'material-theme-palenight'
-      },
-      wrap: true
-    },
-    drafts: true
-  }), sitemap(), tailwind()]
-});
+	output: 'server',
+	adapter: vercel(),
+	site: 'https://technoxdroid.com/',
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+		drafts: true,
+		shikiConfig: {
+			theme: 'material-theme-palenight',
+			wrap: true
+		}
+	},
+	image: {
+		service: {
+			entrypoint: 'astro/assets/services/noop'
+		}
+	},
+	integrations: [
+		mdx({
+			syntaxHighlight: 'shiki',
+			shikiConfig: {
+				experimentalThemes: {
+					light: 'vitesse-light',
+					dark: 'material-theme-palenight'
+				},
+				wrap: true
+			},
+			drafts: true
+		}),
+		sitemap(),
+		tailwind()
+	]
+})
