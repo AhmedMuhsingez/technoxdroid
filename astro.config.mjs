2a13924c6136debf509029d3224aee8d514fc913
AhmedMuhsingez <76662243+AhmedMuhsingez@users.noreply.github.com>
2024-06-03 02:33:46 +0300

    fixed md's h problems....

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,39 +1,44 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import { remarkReadingTime } from './src/utils/readTime.ts';
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import { remarkReadingTime } from './src/utils/readTime.ts'
+import vercel from '@astrojs/vercel/serverless'
 
-import react from "@astrojs/react";
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
-  site: 'https://technoxdroid.com/',
-  // markdown: {
-  // 	remarkPlugins: [remarkReadingTime],
-  // 	drafts: true,
-  // 	shikiConfig: {
-  // 		theme: 'material-theme-palenight',
-  // 		wrap: true
-  // 	}
-  // },
-  // image: {
-  // 	service: {
-  // 		entrypoint: 'astro/assets/services/noop'
-  // 	}
-  // },
-  integrations: [mdx({
-    syntaxHighlight: 'shiki',
-    shikiConfig: {
-      experimentalThemes: {
-        light: 'vitesse-light',
-        dark: 'material-theme-palenight'
-      },
-      wrap: true
-    },
-    drafts: true
-  }), sitemap(), tailwind(), react()]
-});
+	output: 'server',
+	adapter: vercel(),
+	site: 'https://technoxdroid.com/',
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+		drafts: true,
+		shikiConfig: {
+			theme: 'material-theme-palenight',
+			wrap: true
+		}
+	},
+	image: {
+		service: {
+			entrypoint: 'astro/assets/services/noop'
+		}
+	},
+	integrations: [
+		mdx({
+			syntaxHighlight: 'shiki',
+			shikiConfig: {
+				experimentalThemes: {
+					light: 'vitesse-light',
+					dark: 'material-theme-palenight'
+				},
+				wrap: true
+			},
+			drafts: true
+		}),
+		sitemap(),
+		tailwind(),
+		react()
+	]
+})
