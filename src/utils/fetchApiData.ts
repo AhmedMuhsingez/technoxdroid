0d3b1d9daee0ccc68e6b2968273795ef3a3271a7
MrAhmed <76662243+AhmedMuhsingez@users.noreply.github.com>
2024-07-05 13:02:14 +0300

    code clean up

--- src/utils/fetchApiData.ts
+++ src/utils/fetchApiData.ts
@@ -14,34 +14,6 @@ export const fetchArticles = async (page = 1, pageSize = 5, sort = 'createdAt:de
 	return articles
 }
 
-// export const fetchArticlesCards = async (page = 1, pageSize = 5, sort = 'createdAt:desc') => {
-// 	const articles = await fetchApi<Article>({
-// 		endpoint: 'articles',
-// 		query: {
-// 			// populate: ['category', 'cover'].join(','),
-// 			'pagination[page]': String(page),
-// 			'pagination[pageSize]': String(pageSize),
-// 			sort: sort
-// 		}
-// 	})
-
-// 	return articles
-// }
-
-// export const fetchNextPage = async (page: number, pageSize: number, sort: string) => {
-// 	const articles = await fetchApi<Article>({
-// 		endpoint: 'articles',
-// 		query: {
-// 			populate: ['category', 'cover'].join(','),
-// 			'pagination[page]': String(page),
-// 			'pagination[pageSize]': String(pageSize),
-// 			sort: sort
-// 		}
-// 	})
-
-// 	return articles
-// }
-
 export const fetchCategories = async () => {
 	const categories = await fetchApi<Categories>({ endpoint: 'categories' })
 	return categories
