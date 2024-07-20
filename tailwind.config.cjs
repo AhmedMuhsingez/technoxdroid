3d228b24f0593028978ae4d9e6f36c189e21bc47
AhmedMuhsingez <76662243+AhmedMuhsingez@users.noreply.github.com>
2024-07-20 18:14:27 +0300

    navbar shadow fix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -23,7 +23,9 @@ module.exports = {
 				lightCardHover: '0px 6px 16px -12px rgba(0,0,0,0.5)',
 
 				darkCard: '2px 4px 6px 0px rgba(0,0,0,0.6)',
-				darkCardHover: '2px 8px 6px -8px rgba(0,0,0,1)'
+				darkCardHover: '2px 8px 6px -8px rgba(0,0,0,1)',
+
+				navShadow: '0px 10px 12px -4px rgba(0,0,0,0.2)'
 			},
 
 			fontFamily: {
