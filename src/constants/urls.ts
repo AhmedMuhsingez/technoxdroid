179692f5f3bd397d23408c1c72a67a8c9090a149
MrAhmed <76662243+AhmedMuhsingez@users.noreply.github.com>
2024-07-21 17:56:13 +0300

    created footer social icons + fixed navbar

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -9,3 +9,27 @@ export const URLS = {
 		}
 	}
 }
+
+export const SOCIAL_LINKS_FOOTER = [
+	{
+		name: 'Github',
+		url: 'https://github.com/AhmedMuhsingez',
+		icon: 'github',
+		target: '_blank'
+	},
+
+	{
+		name: 'Facebook',
+		// url: 'https://www.facebook.com/TechnoxDroid',
+		url: '',
+		icon: 'facebook',
+		target: '_blank'
+	},
+	{
+		name: 'Instagram',
+		// url: 'https://www.facebook.com/TechnoxDroid',
+		url: '',
+		icon: 'instagram',
+		target: '_blank'
+	}
+]
