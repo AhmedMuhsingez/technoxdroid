0bf014ba99a1f7b47562f357e6e56c8252393075
MrAhmed <76662243+AhmedMuhsingez@users.noreply.github.com>
2024-09-15 13:57:40 +0300

    gap fix

--- src/components/privacy/PrivacyPolicy.tsx
+++ src/components/privacy/PrivacyPolicy.tsx
@@ -33,17 +33,17 @@ function PrivacyPolicy({}: Props) {
 					<div className='flex min-h-full items-center justify-center p-4'>
 						<DialogPanel
 							transition
-							className='forceRTL w-full max-w-lg rounded-xl bg-white/10 dark:bg-white/5 p-6 backdrop-blur-lg duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0'
+							className='forceRTL flex flex-col gap-4 w-full max-w-lg rounded-xl bg-white/10 dark:bg-white/5 p-6 backdrop-blur-lg duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0'
 						>
 							<DialogTitle as='h3' className='md:text-xl font-medium text-white'>
 								الموافقة على استخدام ملفات تعريف الارتباط
 							</DialogTitle>
-							<p className='mt-2 md:text-lg text-md text-white/75'>
+							<p className='md:text-lg text-md text-white/75'>
 								نستخدم ملفات تعريف الارتباط لتحسين تجربتك على الموقع وتقديم محتوى مخصص. يُرجى زيارة
 								صفحة سياسة الخصوصية لمعرفة المزيد حول كيفية استخدامنا لملفات تعريف الارتباط وحماية
 								بياناتك.
 							</p>
-							<div className='mt-4 flex justify-center'>
+							<div className='flex justify-center'>
 								<Button
 									className='inline-flex items-center gap-2 rounded-md py-1.5 px-3 text-md font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-black/10 data-[focus]:outline-1 data-[focus]:outline-white '
 									onClick={handleAccept}
