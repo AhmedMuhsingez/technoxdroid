b91e82baa3a7b8a2e2642682269bb72727c0aa6e
MrAhmed <76662243+AhmedMuhsingez@users.noreply.github.com>
2025-01-03 15:37:08 +0300

    added auth library

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,24 @@ import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel'
 import react from '@astrojs/react'
 
-export default defineConfig({
-	output: 'server',
-	adapter: vercel(),
-	site: 'https://technoxdroid.com/',
+import auth from 'auth-astro';
 
-	markdown: {
-		drafts: true,
-		shikiConfig: {
-			theme: 'material-theme-palenight',
-			wrap: true
-		}
-	},
-	image: {
-		service: {
-			entrypoint: 'astro/assets/services/noop'
-		}
-	},
-	integrations: [sitemap(), tailwind(), react()]
+export default defineConfig({
+    output: 'server',
+    adapter: vercel(),
+    site: 'https://technoxdroid.com/',
+
+    markdown: {
+        drafts: true,
+        shikiConfig: {
+            theme: 'material-theme-palenight',
+            wrap: true
+        }
+    },
+    image: {
+        service: {
+            entrypoint: 'astro/assets/services/noop'
+        }
+    },
+    integrations: [sitemap(), tailwind(), react(), auth()]
 })
